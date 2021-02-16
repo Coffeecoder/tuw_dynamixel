77e351707051d57aee9764a24e71359fd6447bc1
Eugen <eugen.kaltenegger@gmail.com>
2021-02-16 14:32:59 +0100

    implemented wheel demo

--- tuw_dynamixel_demo/src/demo_message_generator_wheel.cpp
+++ tuw_dynamixel_demo/src/demo_message_generator_wheel.cpp
@@ -4,8 +4,32 @@
 
 std::shared_ptr<trajectory_msgs::JointTrajectory> tuw_dynamixel::DemoMessageGeneratorWheel::generateDemoMessage(const std::string actuator_name)
 {
-  // TODO
-  return std::make_shared<trajectory_msgs::JointTrajectory>();
+  std::shared_ptr<trajectory_msgs::JointTrajectory> joint_trajectory = std::make_shared<trajectory_msgs::JointTrajectory>();
+
+  joint_trajectory->joint_names.push_back(actuator_name);
+
+  int acceleration_factor = 0;
+  if (wheel_mode_ == WheelMode::accelerating)
+  {
+    acceleration_factor = 1;
+    wheel_mode_ = WheelMode::decelerating;
+  }
+  if (wheel_mode_ == WheelMode::decelerating) {
+    acceleration_factor = -1;
+    wheel_mode_ = WheelMode::accelerating;
+  }
+
+  std::shared_ptr<trajectory_msgs::JointTrajectoryPoint> joint_trajectory_point_changing = std::make_shared<trajectory_msgs::JointTrajectoryPoint>();
+  joint_trajectory_point_changing->accelerations.push_back(acceleration_factor * 0.25);
+  joint_trajectory_point_changing->time_from_start.fromSec(0);
+  joint_trajectory->points.push_back(*joint_trajectory_point_changing);
+
+  std::shared_ptr<trajectory_msgs::JointTrajectoryPoint> joint_trajectory_point_constant = std::make_shared<trajectory_msgs::JointTrajectoryPoint>();
+  joint_trajectory_point_changing->accelerations.push_back(0.0);
+  joint_trajectory_point_changing->time_from_start.fromSec(3.0);
+  joint_trajectory->points.push_back(*joint_trajectory_point_constant);
+
+  return joint_trajectory;
 }
 
 std::shared_ptr<trajectory_msgs::JointTrajectory> tuw_dynamixel::DemoMessageGeneratorWheel::generateDemoMessage(const YAML::Node yaml_node)
