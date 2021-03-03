6082dd7d81145d9101533f0bdd3e89da9da163c1
Eugen <eugen.kaltenegger@gmail.com>
2021-03-03 13:53:22 +0100

    implmented demo message publisher

--- tuw_dynamixel_demo/include/tuw_dynamixel_demo/demo_message_publisher.h
+++ tuw_dynamixel_demo/include/tuw_dynamixel_demo/demo_message_publisher.h
@@ -5,13 +5,22 @@
 
 #include <ros/ros.h>
 #include <memory>
+#include "demo_message_generator.h"
 
 namespace tuw_dynamixel
 {
 class DemoMessagePublisher
 {
 public:
-  void init();
+  ros::NodeHandle node_handle_;
+  std::string topic_name_;
+  std::string actuator_name_;
+  tuw_dynamixel::DemoMessageGenerator& message_generator_;
+  void init(
+    const ros::NodeHandle& node_handle,
+    const std::string& topic_name,
+    const std::string& actuator_name,
+    const tuw_dynamixel::DemoMessageGenerator& demo_message_generator);
   void start();
 private:
   std::shared_ptr<ros::Publisher> publisher;
--- tuw_dynamixel_demo/src/demo_message_publisher.cpp
+++ tuw_dynamixel_demo/src/demo_message_publisher.cpp
@@ -1,3 +1,30 @@
 // Copyright 2021 Eugen Kaltenegger
 
+#include <trajectory_msgs/JointTrajectory.h>
 #include "../include/tuw_dynamixel_demo/demo_message_publisher.h"
+
+void tuw_dynamixel::DemoMessagePublisher::init(
+    const ros::NodeHandle& node_handle,
+    const std::string& topic_name,
+    const std::string& actuator_name,
+    const tuw_dynamixel::DemoMessageGenerator& message_generator) {
+  this->node_handle_ = node_handle;
+  this->topic_name_ = topic_name;
+  this->actuator_name_ = actuator_name;
+  this->message_generator_ = message_generator;
+
+  this->publisher = std::make_shared<ros::Publisher>(this->node_handle_.advertise<trajectory_msgs::JointTrajectory>(this->topic_name_, 100));
+}
+
+void tuw_dynamixel::DemoMessagePublisher::start() {
+  ROS_INFO("starting demo publisher (100 messages will be published)");
+  int message_counter = 0;
+
+  ros::Rate loop_rate(this->message_generator_.get_message_execution_duration_in_seconds());
+  while (ros::ok() && message_counter < 100) {
+    ROS_INFO("published %3d demo message", message_counter++);
+    std::shared_ptr<trajectory_msgs::JointTrajectory> message = this->message_generator_.generateDemoMessage(this->actuator_name_);
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
+}
